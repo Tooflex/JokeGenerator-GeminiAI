e83417108fa58cec52f6b19c257f9c3fb295d141
Otourou Da Costa <otouroudacosta@gmail.com>
2024-03-22 13:09:13 +0100

    Initial Commit

---
+++ JokeGenerator/JokeGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  JokeGeneratorApp.swift
+//  JokeGenerator
+//
+//  Created by Otourou Da Costa on 22/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct JokeGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
